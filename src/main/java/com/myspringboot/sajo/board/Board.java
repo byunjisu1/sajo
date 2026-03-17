af0172edd5ff2127c6abb5a2ab870cc70613b53a
변지수 <byounjisu0902@gmail.com>
2026-03-17 10:58:52 +0900

    feat: 회원 엔터티 생성

--- src/main/java/com/myspringboot/sajo/board/Board.java
+++ src/main/java/com/myspringboot/sajo/board/Board.java
@@ -1,12 +1,5 @@
 package com.myspringboot.sajo.board;
 
-import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Entity
 public class Board {
 
 }
--- src/main/java/com/myspringboot/sajo/member/Member.java
+++ src/main/java/com/myspringboot/sajo/member/Member.java
@@ -1,5 +1,50 @@
 package com.myspringboot.sajo.member;
 
-public class Member {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+public class Member {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seqMem")
+	@SequenceGenerator(name="seqMem", sequenceName="seq_member_no", allocationSize=1)
+	private Integer member_no;
+	
+	@Column(columnDefinition="VARCHAR2(20 BYTE)")
+	private String nickname;
+	
+	@Column(columnDefinition="VARCHAR2(500 BYTE)")
+	private String email;
+	
+	@Column(columnDefinition="VARCHAR2(20 BYTE)")
+	private String name_kor;
+	
+	@Column(columnDefinition="VARCHAR2(50 BYTE)")
+	private String name_eng;
+	
+	@Column(columnDefinition="VARCHAR2(20 BYTE)")
+	private String birth;
+	
+	@Column(columnDefinition="VARCHAR2(20 BYTE)")
+	private String phone;
+	
+	@Column(columnDefinition="VARCHAR2(500 BYTE)")
+	private String profile_img;
+	
+	@Column(columnDefinition="VARCHAR2(20 BYTE)")
+	private String sns_type;
+	
+	@Column(columnDefinition="VARCHAR2(500 BYTE)")
+	private String access_token;
+	
+	@Column(columnDefinition="VARCHAR2(500 BYTE)")
+	private String customs_id;
 }
