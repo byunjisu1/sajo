0f5df82b22b6d74e78918bb8834a156f230bf8c0
변지수 <byounjisu0902@gmail.com>
2026-03-17 14:27:20 +0900

    test: memberTest 추가

--- src/test/java/com/myspringboot/sajo/MemberTests.java
+++ src/test/java/com/myspringboot/sajo/MemberTests.java
@@ -1,5 +1,29 @@
 package com.myspringboot.sajo;
 
-public class MemberTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.myspringboot.sajo.member.Member;
+import com.myspringboot.sajo.member.MemberRepository;
+
+@SpringBootTest
+public class MemberTests {
+	@Autowired
+	private MemberRepository memberRepo;
+	
+	/**
+	 * testInsertMember : Member 테이블 더미데이터 추가
+	 */
+	@Test
+	void testInsertMember() {
+		Member m = new Member();
+		m.setNickname("김민재");
+		m.setEmail("lim82378@gmail.com");
+		m.setNameKor("김민재");
+		m.setNameEng("KIMMINJAE");
+		m.setBirth("20000402");
+		m.setPhone("01038218237");
+		memberRepo.save(m);
+	}
 }
