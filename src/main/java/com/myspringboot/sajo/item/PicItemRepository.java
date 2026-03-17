061f61543ac12e4a28d5a69c2279daf835ccf667
김민재 <lim82378@gmail.com>
2026-03-17 15:31:02 +0900

    Test: DB 더미데이터 추가

---
+++ src/main/java/com/myspringboot/sajo/item/PicItemRepository.java
@@ -0,0 +1,8 @@
+package com.myspringboot.sajo.item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PicItemRepository extends JpaRepository<PicItem, Integer>{
+
+}
