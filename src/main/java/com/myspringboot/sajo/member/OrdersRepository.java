7a45aae9d226879a9fee9872732cbca377141f90
변지수 <byounjisu0902@gmail.com>
2026-03-17 14:43:01 +0900

    test: member Test 더미데이터 추가

---
+++ src/main/java/com/myspringboot/sajo/member/OrdersRepository.java
@@ -0,0 +1,7 @@
+package com.myspringboot.sajo.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdersRepository extends JpaRepository<Orders, String>{
+
+}
--- src/test/java/com/myspringboot/sajo/MemberTests.java
+++ src/test/java/com/myspringboot/sajo/MemberTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.myspringboot.sajo.member.AddressRepository;
 import com.myspringboot.sajo.member.Member;
 import com.myspringboot.sajo.member.MemberRepository;
 
@@ -11,12 +12,16 @@ import com.myspringboot.sajo.member.MemberRepository;
 public class MemberTests {
 	@Autowired
 	private MemberRepository memberRepo;
+	@Autowired
+	private AddressRepository addressRepo;
+	
 	
 	/**
 	 * testInsertMember : Member 테이블 더미데이터 추가
 	 */
 	@Test
 	void testInsertMember() {
+		// Given
 		Member m = new Member();
 		m.setNickname("김민재");
 		m.setEmail("lim82378@gmail.com");
@@ -24,6 +29,53 @@ public class MemberTests {
 		m.setNameEng("KIMMINJAE");
 		m.setBirth("20000402");
 		m.setPhone("01038218237");
+		
+		// When
 		memberRepo.save(m);
+		
+		// Then
+	}
+	
+	@Test
+	void testInsertMember2() {
+		// Given
+		Member m = new Member();
+		m.setNickname("지수");
+		m.setEmail("0902jisu@naver.com");
+		m.setNameKor("변지수");
+		m.setNameEng("BYUNJISU");
+		m.setBirth("20020902");
+		m.setPhone("01071879350");
+		
+		// When
+		memberRepo.save(m);
+		
+		// Then
+	}
+	
+	@Test
+	void testInsertMember3() {
+		// Given
+		Member m = new Member();
+		m.setNickname("배승빈");
+		m.setEmail("seungbin4369@gmail.com");
+		m.setNameKor("배승빈");
+		m.setNameEng("BAESEUNGBIN");
+		m.setBirth("19960226");
+		m.setPhone("01047056832");
+		
+		// When
+		memberRepo.save(m);
+		
+		// Then
+	}
+	
+	/**
+	 * testInsertAddress : Address 테이블 더미데이터 추가
+	 */
+	@Test
+	void testInsertAddress() {
+		// Given
+		
 	}
 }
