259034b8020e5655ca9a2e42bc014dd635ddd753
배승빈 <seungbin4369@gmail.com>
2026-03-20 16:42:42 +0900

    feat: mypage 만드는중

---
+++ frontend/src/pages/MyPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './MyPage.css';
+
+const MyPage = () => {
+	
+	
+	return(
+		<header className="MyPage-profile-header">
+			<div className="MyPage-profile-info">
+				<span className="MyPage-nickname">지수</span>
+				<span className="MyPage-user-name">변지수 님</span>
+				<span className="MyPage-chevron">〉</span>
+			</div>
+		</header>
+		
+	);
+
+}
+
+export default MyPage;
