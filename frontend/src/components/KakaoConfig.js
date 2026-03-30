a47274f90620d23f8f5f4b4902fd93926c708fa2
배승빈 <seungbin4369@gmail.com>
2026-03-30 17:36:31 +0900

    feat: api키 변경

--- frontend/src/components/KakaoConfig.js
+++ frontend/src/components/KakaoConfig.js
@@ -1,3 +1,3 @@
-export const REST_API_KEY = "37307c06294e5bab4156410a444d51bc";
+export const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
 export const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;	
