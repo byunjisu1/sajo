197c91979f2f1683e3f34d91c827b315675eec98
변지수 <byounjisu0902@gmail.com>
2026-04-03 11:25:28 +0900

    fix

--- src/main/java/com/myspringboot/sajo/bell/BellMsgRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myspringboot.sajo.bell;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BellMsgRepository extends JpaRepository<BellMsg, String> {
-
-}
--- src/main/java/com/myspringboot/sajo/bell/BellRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myspringboot.sajo.bell;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BellRepository extends JpaRepository<Bell, Integer> {
-
-}
